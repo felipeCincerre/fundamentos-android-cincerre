2343c5e9d4cbef70258142cea7ed17c355f31f22
Felipe Cincerre <felipecincerre@hotmail.com>
2015-07-20 18:24:53 -0300

    segundo commit

--- app/src/main/java/com/example/administrador/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrador/myapplication/MainActivity.java
@@ -2,37 +2,31 @@ package com.example.administrador.myapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        TextView minhaView = (TextView) findViewById(R.id.textView);
+        minhaView.setText("Teste de texto");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Button botao = (Button)findViewById(R.id.button);
+        botao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clique no botao!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
