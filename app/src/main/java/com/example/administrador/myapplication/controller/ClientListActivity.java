a0cb503f0753073065032fb516ddf59367b53fd2
Felipe Cincerre <felipecincerre@hotmail.com>
2015-07-29 21:38:03 -0300

    fim treinamento

--- app/src/main/java/com/example/administrador/myapplication/controller/ClientListActivity.java
+++ app/src/main/java/com/example/administrador/myapplication/controller/ClientListActivity.java
@@ -3,6 +3,7 @@ package com.example.administrador.myapplication.controller;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,9 @@ import android.widget.Toast;
 
 import com.example.administrador.myapplication.model.entities.Client;
 import com.example.administrador.myapplication.R;
+import com.melnykov.fab.FloatingActionButton;
+
+import org.apache.http.protocol.HTTP;
 
 import java.util.List;
 
@@ -23,6 +27,7 @@ import java.util.List;
 public class ClientListActivity extends AppCompatActivity {
     private ListView listViewClients;
     private Client client;
+    private FloatingActionButton fabAdd;
     private static final String TAG = ClientListActivity.class.getSimpleName();
 
     @Override
@@ -30,7 +35,19 @@ public class ClientListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindClientList();
+        bindFab();
 
+    }
+
+    private void bindFab() {
+        fabAdd = (FloatingActionButton) findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMainActivity = new Intent(ClientListActivity.this, ClientPersistActivity.class);
+                startActivity(goToMainActivity);
+            }
+        });
     }
 
     private void bindClientList() {
@@ -45,6 +62,16 @@ public class ClientListActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 client = (Client) parent.getItemAtPosition(position);
                 return false;
+            }
+        });
+        listViewClients.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Client client = (Client) parent.getItemAtPosition(position);
+                // Best Practices: http://stackoverflow.com/questions/4275678/how-to-make-phone-call-using-intent-in-android
+                final Intent goToSOPhoneCall = new Intent(Intent.ACTION_DIAL /* or Intent.ACTION_DIAL (no manifest permission needed) */);
+                goToSOPhoneCall.setData(Uri.parse("tel:" + client.getPhone()));
+                startActivity(goToSOPhoneCall);
             }
         });
         registerForContextMenu(listViewClients);
@@ -104,8 +131,19 @@ public class ClientListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menuAdd) {
-            Intent goToMainActivity = new Intent(ClientListActivity.this, ClientPersistActivity.class);
-            startActivity(goToMainActivity);
+            // Create the text message with a string
+            final Intent sendIntent = new Intent(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Nao me diga mentirinhas, Fabricio");
+            sendIntent.setType(HTTP.PLAIN_TEXT_TYPE);
+
+            // Create intent to show the chooser dialog
+            final Intent chooser = Intent.createChooser(sendIntent, "Titulo Chooser");
+
+            // Verify the original intent will resolve to at least one activity
+            if (sendIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(chooser);
+            }
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
